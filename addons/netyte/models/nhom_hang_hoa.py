f2973d1321d2fbea46419fa4f75b5ba8691353d6
tutt1999nd1 <43142674+tutt1999nd1@users.noreply.github.com>
2020-07-04 23:07:44 +0700

    Số thứ tự Tìm kiếm theo các trường

--- addons/netyte/models/nhom_hang_hoa.py
+++ addons/netyte/models/nhom_hang_hoa.py
@@ -7,6 +7,6 @@ class Nhom_hang_hoa(models.Model):
     status = fields.Selection([
         ('dangkinhdoanh', 'Đang kinh doanh'),
         ('ngungkinhdoanh', 'Ngừng kinh doanh')
-    ], string='Trạng thái', default='Đang kinh doanh')
+    ], string='Trạng thái', default='dangkinhdoanh')
     description = fields.Char('Mô tả')
     parent_id = fields.Many2one('nhom.hang.hoa', string='Cấp cha')
